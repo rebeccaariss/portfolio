093629d8d8b25fb1474904966f88a9ffc26019c7
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-03-08 22:48:56 -0500

    Set up sections and basic CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <nav>
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#projects">projects</a>
+        <a href="#contact">contact</a>
+      </nav>
+      <section id="home">
+        <h1>Hi! I'm Rebecca.</h1>
+      </section>
+      <section id="about">
+        <h2>This is an h2</h2>
+      </section>
+      <section id="projects">
+        <p>This is a paragraph</p>
+      </section>
+      <section id="contact">
+        {/* icons */}
+      </section>
     </div>
   );
 }
