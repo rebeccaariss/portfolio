d127cf4c4b60359c09ff0d620bedd4dc7f41dd0a
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-03-20 23:51:19 -0400

    Adjust 'About' content for mobile

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,21 @@ import React from 'react';
 function About () {
   return (
     <section id='about'>
-      <h2>A little bit about me:</h2>
+      <div className='content'>
+        <h2>A little bit about me:</h2>
 
-      <p>🩺 Background in healthcare and hospitality</p>
+        <p>🩺 Background in healthcare and hospitality</p>
 
-      <p>🎓 <a href='https://www.lighthouselabs.ca/en/employers' target='_blank' rel='noopener noreferrer'>Lighthouse Labs</a> grad</p>
+        <p>🎓 <a href='https://www.lighthouselabs.ca/en/employers' target='_blank' rel='noopener noreferrer'>Lighthouse Labs</a> grad</p>
 
-      <p>👩🏼‍💻 Currently studying: <a href='https://www.udemy.com/course/complete-react-developer-zero-to-mastery/?couponCode=KEEPLEARNING' target='_blank' rel='noopener noreferrer'>Redux</a>, <a href='https://www.udacity.com/course/web-accessibility--ud891' target='_blank' rel='noopener noreferrer'>Web Accessibility</a></p>
+        <p>👩🏼‍💻 Currently studying: <a href='https://www.udemy.com/course/complete-react-developer-zero-to-mastery/?couponCode=KEEPLEARNING' target='_blank' rel='noopener noreferrer'>Redux</a>, <a href='https://www.udacity.com/course/web-accessibility--ud891' target='_blank' rel='noopener noreferrer'>Web Accessibility</a></p>
 
-      <p>🎧 Listening to: <a href='https://open.spotify.com/artist/2mVVjNmdjXZZDvhgQWiakk' target='_blank' rel='noopener noreferrer'>Khruangbin</a>, <a href='https://open.spotify.com/artist/3IunaFjvNKj98JW89JYv9u' target='_blank' rel='noopener noreferrer'>The Japanese House</a></p>
+        <p>🎧 Listening to: <a href='https://open.spotify.com/artist/2mVVjNmdjXZZDvhgQWiakk' target='_blank' rel='noopener noreferrer'>Khruangbin</a>, <a href='https://open.spotify.com/artist/3IunaFjvNKj98JW89JYv9u' target='_blank' rel='noopener noreferrer'>The Japanese House</a></p>
 
-      <p>📖 Reading: <a href='https://www.goodreads.com/en/book/show/57945316' target='_blank' rel='noopener noreferrer'>Babel</a> by R.F. Kuang, <a href='https://www.goodreads.com/book/show/52905471-why-we-swim?ref=nav_sb_ss_1_11' target='_blank' rel='noopener noreferrer'>Why We Swim</a> by Bonnie Tsui</p>
+        <p>📖 Reading: <a href='https://www.goodreads.com/en/book/show/57945316' target='_blank' rel='noopener noreferrer'>Babel</a> by R.F. Kuang, <a href='https://www.goodreads.com/book/show/52905471-why-we-swim?ref=nav_sb_ss_1_11' target='_blank' rel='noopener noreferrer'>Why We Swim</a> by Bonnie Tsui</p>
 
-      <p>🎮 Playing: <a href='https://www.nintendo.com/en-ca/store/products/eastward-octopia-70050000042973-switch/' target='_blank' rel='noopener noreferrer'>Octopia</a>, <a href='https://www.playstation.com/en-ca/games/kena-bridge-of-spirits/' target='_blank' rel='noopener noreferrer'>Kena: Bridge of Spirits</a></p>
+        <p>🎮 Playing: <a href='https://www.nintendo.com/en-ca/store/products/eastward-octopia-70050000042973-switch/' target='_blank' rel='noopener noreferrer'>Octopia</a>, <a href='https://www.playstation.com/en-ca/games/kena-bridge-of-spirits/' target='_blank' rel='noopener noreferrer'>Kena: Bridge of Spirits</a></p>
+      </div>
     </section>
   );
 };
