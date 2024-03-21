2e83f71e0f2e2f690081095977aff654e22aa82d
Rebecca Ariss <rebeccamarionruth@gmail.com>
2024-03-20 22:53:53 -0400

    Fix gif imports

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import karityDemo from '../assets/karity-demo.gif';
+import linkfolioDemo1 from '../assets/linkfolio-demo-1.gif';
+// import linkfolioDemo2 from '../assets/linkfolio-demo-2.gif';
+import photoLabsDemo from '../assets/photolabs-demo.gif';
 
 function Projects() {
   return (
     <section id='projects'>
-      <h2>🔧 <em>This section is under construction</em> 🚧</h2>
-      {/* <div id='karity'>
+      {/* <h2>🔧 <em>This section is under construction</em> 🚧</h2> */}
+      <div id='karity'>
         <h2>Karity</h2>
-        <img src='/assets/karity-demo.gif' alt='Karity Demo'/>
+        <img src={karityDemo} alt='Karity Demo'/>
       </div>
       <div id='linkfolio'>
         <h2>Linkfolio</h2>
-        <img src='/assets/linkfolio-demo-1.gif' alt='Linkfolio Demo 1'/>
-        <img src='/assets/linkfolio-demo-2.gif' alt='Linkfolio Demo 2'/>
+        <img src={linkfolioDemo1} alt='Linkfolio Demo 1'/>
+        {/* <img src={linkfolioDemo2} alt='Linkfolio Demo 2'/> */}
       </div>
       <div id='photolabs'>
         <h2>PhotoLabs</h2>
-        <img src='/assets/photolabs-demo.gif' alt='Photolabs Demo'/>
-      </div> */}
+        <img src={photoLabsDemo} alt='Photolabs Demo'/>
+      </div>
     </section>
   );
 };
