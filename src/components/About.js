a4a4b9ecc14278a2256453b06c905d01a8ec36e7
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2024-10-07 23:30:10 -0400

    Update About page

--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,13 @@ function About () {
 
         <p>🎓 <a href='https://www.lighthouselabs.ca/en/employers' target='_blank' rel='noopener noreferrer'>Lighthouse Labs</a> grad</p>
 
-        <p>👩🏼‍💻 Currently studying: <a href='https://nextjs.org/learn' target='_blank' rel='noopener noreferrer'>Next.js</a>, <a href='https://www.udacity.com/course/web-accessibility--ud891' target='_blank' rel='noopener noreferrer'>Web Accessibility</a></p>
+        <p>💜 Interned at <a href='https://purposely.co/' target='_blank' rel='noopener noreferrer'>Purposely</a> (and loved it)</p>
+
+        <p>👩🏼‍💻 Currently studying: <a href='https://docs.python.org/3/' target='_blank' rel='noopener noreferrer'>Python</a>, <a href='https://www.djangoproject.com/' target='_blank' rel='noopener noreferrer'>Django</a>, and <a href='https://www.django-rest-framework.org/' target='_blank' rel='noopener noreferrer'>Django REST</a></p>
 
         <p>🎧 Listening to: <a href='https://open.spotify.com/artist/2mVVjNmdjXZZDvhgQWiakk' target='_blank' rel='noopener noreferrer'>Khruangbin</a>, <a href='https://open.spotify.com/artist/3IunaFjvNKj98JW89JYv9u' target='_blank' rel='noopener noreferrer'>The Japanese House</a></p>
 
-        <p>📖 Reading: <a href='https://www.goodreads.com/en/book/show/57945316' target='_blank' rel='noopener noreferrer'>Babel</a> by R.F. Kuang, <a href='https://www.goodreads.com/book/show/52905471-why-we-swim?ref=nav_sb_ss_1_11' target='_blank' rel='noopener noreferrer'>Why We Swim</a> by Bonnie Tsui</p>
+        <p>📖 Reading: <a href='https://www.goodreads.com/book/show/89717.The_Haunting_of_Hill_House' target='_blank' rel='noopener noreferrer'>The Haunting of Hill House</a> by Shirley Jackson, <a href='https://www.goodreads.com/book/show/62608040-fieldwork' target='_blank' rel='noopener noreferrer'>Fieldwork: A Forager's Memoir</a> by Iliana Regan</p>
 
         <p>🎮 Playing: <a href='https://www.nintendo.com/en-ca/store/products/eastward-octopia-70050000042973-switch/' target='_blank' rel='noopener noreferrer'>Octopia</a>, <a href='https://www.playstation.com/en-ca/games/kena-bridge-of-spirits/' target='_blank' rel='noopener noreferrer'>Kena: Bridge of Spirits</a></p>
       </div>
