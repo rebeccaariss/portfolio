04013b5ea7ff35f07c73c301c2d4a2434d34890e
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2024-10-08 20:00:28 -0400

    Add location for consistency with GitHub, add updates, add skills icons for later

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,8 @@ function About () {
       <div className='content'>
         <h2>A little bit about me:</h2>
 
+        <p>📍 Toronto == home</p>
+
         <p>🩺 Background in healthcare and hospitality</p>
 
         <p>🎓 <a href='https://www.lighthouselabs.ca/en/employers' target='_blank' rel='noopener noreferrer'>Lighthouse Labs</a> grad</p>
@@ -14,12 +16,44 @@ function About () {
 
         <p>👩🏼‍💻 Currently studying: <a href='https://docs.python.org/3/' target='_blank' rel='noopener noreferrer'>Python</a>, <a href='https://www.djangoproject.com/' target='_blank' rel='noopener noreferrer'>Django</a>, and <a href='https://www.django-rest-framework.org/' target='_blank' rel='noopener noreferrer'>Django REST</a></p>
 
-        <p>🎧 Listening to: <a href='https://open.spotify.com/artist/2mVVjNmdjXZZDvhgQWiakk' target='_blank' rel='noopener noreferrer'>Khruangbin</a>, <a href='https://open.spotify.com/artist/3IunaFjvNKj98JW89JYv9u' target='_blank' rel='noopener noreferrer'>The Japanese House</a></p>
+        <p>🎧 Listening to: <a href='https://open.spotify.com/artist/5QdyldG4Fl4TPiOIeMNpBZ?si=7-KUc6_oSR-_y3Tp8GEaRA' target='_blank' rel='noopener noreferrer'>Big Thief</a>, <a href='https://open.spotify.com/artist/3IunaFjvNKj98JW89JYv9u' target='_blank' rel='noopener noreferrer'>The Japanese House</a></p>
 
         <p>📖 Reading: <a href='https://www.goodreads.com/book/show/89717.The_Haunting_of_Hill_House' target='_blank' rel='noopener noreferrer'>The Haunting of Hill House</a> by Shirley Jackson, <br></br><a href='https://www.goodreads.com/book/show/62608040-fieldwork' target='_blank' rel='noopener noreferrer'>Fieldwork: A Forager's Memoir</a> by Iliana Regan</p>
 
-        <p>🎮 Playing: <a href='https://www.nintendo.com/en-ca/store/products/eastward-octopia-70050000042973-switch/' target='_blank' rel='noopener noreferrer'>Octopia</a>, <a href='https://www.playstation.com/en-ca/games/kena-bridge-of-spirits/' target='_blank' rel='noopener noreferrer'>Kena: Bridge of Spirits</a></p>
+        <p>🎮 Playing: <a href='https://www.nintendo.com/us/store/products/gone-home-switch/' target='_blank' rel='noopener noreferrer'>Gone Home</a>, <a href='https://www.playstation.com/en-ca/games/kena-bridge-of-spirits/' target='_blank' rel='noopener noreferrer'>Kena: Bridge of Spirits</a></p>
       </div>
+
+      {/* <p align="center">
+        <a href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg" className='icon' alt="Git" /></a>
+        
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg" className='icon' alt="React" /></a>
+        
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg" className='icon' alt="JavaScript" /></a>
+        
+        <a href="https://www.python.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg" className='icon' alt="Python" /></a>
+        
+        <a href="https://www.ruby-lang.org/en/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/ruby-colored.svg" className='icon' alt="Ruby" /></a>
+          
+        <a href="https://jquery.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/jquery-colored.svg" className='icon' alt="JQuery" /></a>
+        
+        <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" alt="HTML5" /></a>
+        
+        <a href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg" className='icon' alt="CSS3" /></a>
+        
+        <a href="https://sass-lang.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg" className='icon' alt="Sass" /></a>
+          
+        <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg" className='icon' alt="NodeJS" /></a>
+        
+        <a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg" className='icon' alt="Express" /></a>
+          
+        <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg" className='icon' alt="PostgreSQL" /></a>
+        
+        <a href="https://getbootstrap.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg" className='icon' alt="Bootstrap" /></a>
+        
+        <a href="https://mui.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/materialui-colored.svg" className='icon' alt="Material UI" /></a>
+        
+        <a href="https://www.figma.com/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/figma-colored.svg" className='icon' alt="Figma" /></a>
+      </p> */}
     </section>
   );
 };
