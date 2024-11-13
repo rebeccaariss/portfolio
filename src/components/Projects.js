8ade3cba1dd7ba4273754c29508cb00d7fa82aa0
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2024-11-12 23:24:18 -0500

    Map over CONTENT for cleaner component & fewer lines of code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -59,61 +59,21 @@ const CONTENT = [
 function Projects() {
   return (
     <section id='projects'>
-      <section id='matchymatchy'>
-        <a href='https://github.com/rebeccaariss/matchy-matchy' target='blank' rel='noopener noreferrer' alt='matchy matchy GitHub Repo'>
-          <h2>matchy matchy</h2>
-        </a>
-        <br></br>
-        <a href='https://github.com/rebeccaariss/matchy-matchy' target='blank' rel='noopener noreferrer' alt='matchy matchy GitHub Repo'>
-          <img src={matchyMatchyDemo} alt='matchy matchy Demo'/>
-        </a>
-        <br></br>
-        <p>🚧 <em>Work in progress</em> 🚧</p>
-        <br></br>
-        <p>A minimalist and nostalgic card-matching game featuring some of my favourite emojis. You can follow along for progress updates in my dev log <a href='https://github.com/rebeccaariss/matchy-matchy/blob/main/README.md' target='blank' rel='noopener noreferrer' alt='matchy matchy README'>here!</a></p>
-      </section>
-
-      <section id='karity'>
-        <a href='https://github.com/rebeccaariss/charity-registry' target='blank' rel='noopener noreferrer' alt='Karity GitHub Repo'>
-          <h2>Karity</h2>
-        </a>
-        <br></br>
-        <a href='https://github.com/rebeccaariss/charity-registry' target='blank' rel='noopener noreferrer' alt='Karity GitHub Repo'>
-          <img src={karityDemo} alt='Karity Demo'/>
-        </a>
-        <br></br>
-        <strong><p>Lighthouse Labs Final Team Project: Karity – A Charity & Nonprofit Requests App</p></strong>
-        <br></br>
-        <p>Organizations specify exactly what they need (items, money, volunteers); users can fulfill requests and follow their favourite organizations for updates.</p>
-      </section>
-
-      <section id='linkfolio'>
-        <a href='https://github.com/rebeccaariss/resource-wall' target='blank' rel='noopener noreferrer' alt='Linkfolio GitHub Repo'>
-          <h2>Linkfolio</h2>
-        </a>
-        <br></br>
-        <a href='https://github.com/rebeccaariss/resource-wall' target='blank' rel='noopener noreferrer' alt='Linkfolio GitHub Repo'>
-          <img src={linkfolioDemo1} alt='Linkfolio Demo 1'/>
-        </a>
-        <br></br>
-        <strong><p>Lighthouse Labs Team Project: Linkfolio</p></strong>
-        <br></br>
-        <p>A Pinterest clone for archiving educational resources.</p>
-      </section>
-
-      <section id='photolabs'>
-        <a href='https://github.com/rebeccaariss/photolabs-starter' target='blank' rel='noopener noreferrer' alt='PhotoLabs GitHub Repo'>
-          <h2>PhotoLabs</h2>
-        </a>
-        <br></br>
-        <a href='https://github.com/rebeccaariss/photolabs-starter' target='blank' rel='noopener noreferrer' alt='PhotoLabs GitHub Repo'>
-          <img src={photoLabsDemo} alt='Photolabs Demo'/>
-        </a>
-        <br></br>
-        <strong><p>PhotoLabs</p></strong>
-        <br></br>
-        <p>React SPA (single-page application) for viewing and liking photos, viewing similar photos, and viewing photos by category.</p>
-      </section>
+      {CONTENT.map(({ id, title, link, linkAlt, img, imgAlt, text }) => (
+        <section id={id}>
+          <a href={link} target='blank' rel='noopener noreferrer' alt={linkAlt}>
+            <h2>{title}</h2>
+          </a>
+          <br></br>
+          <a href={link} target='blank' rel='noopener noreferrer' alt={linkAlt}>
+            <img src={img} alt={imgAlt}/>
+          </a>
+          <br></br>
+          {text[0]}
+          <br></br>
+          {text[1]}
+        </section>
+      ))}
     </section>
   );
 };
